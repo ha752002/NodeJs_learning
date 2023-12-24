5beb1f805ed48f3294eeb7abf8c925aa87cbb543
ha752002 <honggha75@gmail.com>
2023-12-24 16:10:36 +0700

    add trigger

--- day_52/day52.sql
+++ day_52/day52.sql
@@ -1,6 +1,9 @@
---
+
 CREATE DATABASE database_01_hongha;
---
+
+ALTER DATABASE database_01_hongha SET timezone TO 'Asia/Ho_Chi_Minh';
+SET search_path = database_01_hongha;
+
 CREATE TABLE courses (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
@@ -8,9 +11,25 @@ CREATE TABLE courses (
   detail TEXT,
   teacher_id INT NOT NULL,
   active INT,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+--
+CREATE FUNCTION updated_at_function()
+RETURNS trigger AS
+$$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+--
+-- DROP TRIGGER IF EXISTS courses_moddatetime ON courses CASCADE;
+CREATE TRIGGER courses_updated_at
+    BEFORE UPDATE ON courses
+    FOR EACH ROW
+    EXECUTE PROCEDURE updated_at_function();
+
 --
 ALTER TABLE courses ADD description TEXT NULL;
 --
@@ -20,28 +39,34 @@ CREATE TABLE teacher (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
   bio TEXT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+--
 ALTER TABLE courses
       ADD CONSTRAINT fk_course_teacher FOREIGN KEY (teacher_id) 
           REFERENCES teacher (id);
+--
+CREATE TRIGGER teacher_updated_at
+    BEFORE UPDATE ON teacher
+    FOR EACH ROW
+    EXECUTE PROCEDURE updated_at_function();
 --
 INSERT INTO teacher (name, bio) VALUES
   ('Teacher 1', 'Bio 1'),
   ('Teacher 2', 'Bio 2'),
   ('Teacher 3', 'Bio 3');
 --
-INSERT INTO courses (name, price, teacher_id, active, created_at, updated_at) VALUES
-  ('Course 1', 100000, 1, 1, NOW(), NOW()),
-  ('Course 2', 200000, 1, 1, NOW(), NOW()),
-  ('Course 3', 300000, 1, 1, NOW(), NOW()),
-  ('Course 4', 100000, 2, 1, NOW(), NOW()),
-  ('Course 5', 200000, 2, 1, NOW(), NOW()),
-  ('Course 6', 300000, 2, 1, NOW(), NOW()),
-  ('Course 7', 100000, 3, 1, NOW(), NOW()),
-  ('Course 8', 200000, 3, 1, NOW(), NOW()),
-  ('Course 9', 300000, 3, 1, NOW(), NOW());
+INSERT INTO courses (name, price, teacher_id, active) VALUES
+  ('Course 10', 100000, 1, 1),
+  ('Course 2', 200000, 1, 1),
+  ('Course 3', 300000, 1, 1),
+  ('Course 4', 100000, 2, 1),
+  ('Course 5', 200000, 2, 1),
+  ('Course 6', 300000, 2, 1),
+  ('Course 7', 100000, 3, 1),
+  ('Course 8', 200000, 3, 1),
+  ('Course 9', 300000, 3, 1);
 --
 UPDATE courses SET name = 'New Course 1', price = 200000 WHERE id = 1;
 UPDATE courses SET name = 'New Course 2', price = 300000 WHERE id = 2;
