17d38440bbcb2bc619065ba4a121e3af59c41e97
ha752002 <honggha75@gmail.com>
2024-01-03 16:51:38 +0700

    day_55

---
+++ day_55/controllers/user.controller.js
@@ -0,0 +1,86 @@
+const userModel = require('../models/user.model')
+const moment = require('moment')
+const { object, string } = require('yup')
+module.exports = {
+  index: async (req, res) => {
+    const { status, keyword } = req.query
+    let statusBool
+    if (status === 'active' || status === 'inactive') {
+      statusBool = status === 'active' ? true : false
+    }
+
+    //Đọc dữ liệu từ database
+    const users = await userModel.all(statusBool, keyword)
+    const msg = req.flash('msg')
+    res.render('users/index', { users, moment, status, keyword, msg })
+  },
+  add: (req, res) => {
+    // const errors = req.flash("errors");
+    // console.log(errors);
+    // console.log(req.errors);
+    res.render('users/add', { req })
+  },
+  handleAdd: async (req, res) => {
+    const schema = object({
+      name: string().required('Tên bắt buộc phải nhập'),
+      email: string()
+        .required('Email bắt buộc phải nhập')
+        .email('Email không đúng định dạng')
+        .test('unique', 'Email đã tồn tại trên hệ thống', async (value) => {
+          return await userModel.emailUnique(value)
+        })
+    })
+    try {
+      const body = await schema.validate(req.body, { abortEarly: false })
+      body.status = body.status === '1' ? true : false
+      await userModel.create(body)
+      req.flash('msg', 'Thêm người dùng thành công')
+      return res.redirect('/users')
+    } catch (e) {
+      const errors = Object.fromEntries(
+        e?.inner.map((item) => [item.path, item.message])
+      )
+      req.flash('errors', errors)
+      req.flash('old', req.body)
+    }
+    return res.redirect('/users/add')
+  },
+  edit: async (req, res) => {
+    const { id } = req.params
+    const user = await userModel.find(id)
+    res.render('users/edit', { user, req })
+  },
+  handleEdit: async (req, res) => {
+    const { id } = req.params
+    const schema = object({
+      name: string().required('Tên bắt buộc phải nhập'),
+      email: string()
+        .required('Email bắt buộc phải nhập')
+        .email('Email không đúng định dạng')
+        .test('unique', 'Email đã tồn tại trên hệ thống', async (value) => {
+          return await userModel.emailUnique(value, id)
+        })
+    })
+    try {
+      const body = await schema.validate(req.body, { abortEarly: false })
+      body.status = body.status === '1' ? true : false
+      await userModel.update(id, body)
+      req.flash('msg', 'Cập nhật người dùng thành công')
+      return res.redirect('/users')
+    } catch (e) {
+      const errors = Object.fromEntries(
+        e?.inner.map((item) => [item.path, item.message])
+      )
+      req.flash('errors', errors)
+      req.flash('old', req.body)
+    }
+    return res.redirect(`/users/edit/${id}`)
+  },
+  delete: async (req, res) => {
+    const { id } = req.params
+    console.log(id)
+    await userModel.delete(id)
+    req.flash('msg', 'Xóa người dùng thành công')
+    res.redirect('/users')
+  }
+}
---
+++ day_55/models/user.model.js
@@ -0,0 +1,47 @@
+//Tư duy model
+/*
+Mỗi model tương ứng 1 table
+Trong 1 controller có thể có nhiều model
+*/
+const sql = require('../utils/db')
+module.exports = {
+  all: (status, keyword) => {
+    let filter = sql`WHERE name IS NOT NULL`
+
+    if (status !== undefined) {
+      filter = sql`${filter} AND status = ${status}`
+    }
+    if (keyword?.length) {
+      filter = sql`${filter} AND (LOWER(name) LIKE ${
+        '%' + keyword + '%'
+      } OR LOWER(email) LIKE ${'%' + keyword + '%'})`
+    }
+
+    return sql`SELECT * FROM users ${filter} ORDER BY created_at DESC`
+  },
+  emailUnique: async (email, id) => {
+    if (id) {
+      const result =
+        await sql`SELECT id FROM users WHERE email=${email} AND id<>${id}`
+      return result.length ? false : true
+    } else {
+      const result = await sql`SELECT id FROM users WHERE email=${email}`
+      return result.length ? false : true
+    }
+    //Nếu email tồn tại -> false - Ngược lại true
+  },
+  create: (data) => {
+    const { name, email, status } = data
+    return sql`INSERT INTO users(name, email, status) VALUES(${name}, ${email}, ${status})`
+  },
+  find: (id) => {
+    return sql`SELECT * FROM users WHERE id=${id}`
+  },
+  update: (id, data) => {
+    const { name, email, status } = data
+    return sql`UPDATE users SET name=${name}, email=${email}, status=${status} WHERE id=${id}`
+  },
+  delete: (id) => {
+    return sql`DELETE FROM users WHERE id=${id}`
+  }
+}
