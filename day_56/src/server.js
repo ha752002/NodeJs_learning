2eacfef3e5845c08163ea1151df1f50062ce1f12
ha752002 <honggha75@gmail.com>
2024-01-06 17:40:48 +0700

    day56

--- day_56/src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-console.log(1000);
-var app = express();
-console.log(10);
-app.listen(3000, function () {
-    console.log("[server]: Server is running at http://localhost:".concat(3000));
-});
