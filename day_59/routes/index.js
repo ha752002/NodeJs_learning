ba0be659c5c4321055e8027909416d216353c763
ha752002 <honggha75@gmail.com>
2024-01-17 09:17:20 +0700

    update

--- day_59/routes/index.js
+++ day_59/routes/index.js
@@ -15,7 +15,7 @@ router.get('/mailTracker/:id', async function (req, res, next) {
     mail.status = true
     await mail.save()
   }
-  res.sendFile('trackingImage.png', { root: '../../public/images' })
+  res.sendFile('trackingImage.png', { root: process.cwd() })
   // next()
   // res.send(mail)
 })
